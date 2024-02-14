5d9e4a72d8aa6315b79cfd33f4640a666c2e3ed8
jamie <jamie.michel@notifi.network>
2024-02-14 02:23:21 +0100

    fix: sanitize file paths of parsers, use correct commonjs rollup pkg

--- assets/baseparsers/evm-parser/rollup.config.js
+++ assets/baseparsers/evm-parser/rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
 
 export default {
